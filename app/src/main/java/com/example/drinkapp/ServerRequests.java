f325568aead675d4261dbab6d4eee050685002bc
Simon <Simon.bjergoe@gmail.com>
2020-05-17 11:56:02 +0200

    Network request on sign up

---
+++ app/src/main/java/com/example/drinkapp/ServerRequests.java
@@ -0,0 +1,46 @@
+package com.example.drinkapp;
+
+import android.os.AsyncTask;
+
+import androidx.annotation.Nullable;
+
+import com.google.gson.Gson;
+
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+
+public class ServerRequests<T> extends AsyncTask {
+    T object;
+    Callback callback;
+    ServerRequests(@Nullable T object, Callback callback)
+    {
+        this.object = object;
+        this.callback = callback;
+    }
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        try {
+            MediaType JSON = MediaType.get("application/json; charset=utf-8");
+            Gson gson = new Gson();
+
+            String userJson = gson.toJson(this.object);
+            OkHttpClient client = new OkHttpClient();
+
+            RequestBody body = RequestBody.create(userJson, JSON);
+            Request req = new Request.Builder()
+                    .url("http://10.0.1.30:8080/user/register")
+                    .post(body)
+                    .build();
+
+            client.newCall(req).enqueue(this.callback);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
+
--- app/src/main/java/com/example/drinkapp/SignUpActivity.java
+++ app/src/main/java/com/example/drinkapp/SignUpActivity.java
@@ -1,15 +1,19 @@
 package com.example.drinkapp;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+
+import org.jetbrains.annotations.NotNull;
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
     EditText signupUsername, signUpPassword;
@@ -37,5 +41,36 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
             startActivity(returnToLogin);
             finish();
         }
+        if (view == btSignUpToServer) {
+            Login minven = new Login(signupUsername.getText().toString(), signUpPassword.getText().toString());
+            ServerRequests<Login> sr = new ServerRequests<>(null, new signUpCallBack());
+            sr.execute();
+            Intent goBackToLogin = new Intent(this, MainActivity.class);
+            startActivity(goBackToLogin);
+            finish();
+        }
+    }
+    class Login {
+        String mail;
+        String password;
+
+        Login(String uname, String pword) {
+            mail = uname;
+            password = pword;
+        }
+    }
+    class signUpCallBack implements Callback {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                e.printStackTrace();
+                System.out.println("hello");
+                call.cancel();
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                System.out.println(response.body().string());
+                System.out.println("asdasd");
+            }
     }
 }
