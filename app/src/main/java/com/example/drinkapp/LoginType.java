92e8dba68d02d1c64309da1502ba6eb612d00832
Bastian Inuk Christensen <bastian@inuk.blog>
2020-05-23 12:35:05 +0200

    Added static initilisers

--- app/src/main/java/com/example/drinkapp/LoginType.java
+++ app/src/main/java/com/example/drinkapp/LoginType.java
@@ -1,8 +1,41 @@
 package com.example.drinkapp;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.Base64;
+
 public enum LoginType {
     Token,
     Basic;
 
-    String hash;
+    public String hash;
+
+    private LoginType() { }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    static LoginType basic(String username, String password) {
+        LoginType login = Basic;
+
+        String loginString = username + ":" + password;
+        login.hash = Base64
+                .getEncoder()
+                .withoutPadding()
+                .encodeToString(loginString.getBytes());
+
+        return login;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    static LoginType token(String token) {
+        LoginType login =  Token;
+
+        login.hash = Base64
+                .getEncoder()
+                .withoutPadding()
+                .encodeToString(token.getBytes());
+
+        return login;
+    }
 }
