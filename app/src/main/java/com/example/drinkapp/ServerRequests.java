ceb566aa41c2da2f306c70b9b2eb24fec8078c6c
Bastian Inuk Christensen <bastian@inuk.blog>
2020-06-02 15:10:59 +0200

    Added comments

--- app/src/main/java/com/example/drinkapp/ServerRequests.java
+++ app/src/main/java/com/example/drinkapp/ServerRequests.java
@@ -21,26 +21,35 @@ public class ServerRequests<T> extends AsyncTask {
     HTTPRequestType requestType;
 
     ServerRequests(String path, @Nullable T object, @Nullable LoginType loginType, Callback callback, HTTPRequestType requestType) {
+        // Payload
         this.object = object;
+        // The class to be called when the request has executed
         this.callback = callback;
+        // The given auth type used for performing user restricted requests
         this.loginType = loginType;
+        // The endpoint at which the request is performed against
         this.path = path;
+        // The semantic HTTP request
         this.requestType = requestType;
     }
 
     @Override
     protected Object doInBackground(Object[] objects) {
         try {
+            // Prepare server for the incomming media type
             MediaType JSON = MediaType.get("application/json; charset=utf-8");
             Gson gson = new Gson();
 
+            // Turn the payload into a json object
             String userJson = gson.toJson(this.object);
             OkHttpClient client = new OkHttpClient();
 
+            // Begin the request builder with the path
             RequestBody body = RequestBody.create(userJson, JSON);
             Request.Builder reqBuilder = new Request.Builder()
                     .url("http://192.168.0.14/" + path);
 
+            // Set the HTTP request type
             switch (requestType) {
                 case Get:
                     reqBuilder = reqBuilder.get();
@@ -52,6 +61,7 @@ public class ServerRequests<T> extends AsyncTask {
                     break;
             }
 
+            // If there's a login type, then put it on the request builder
             if (loginType != null) {
                 switch (loginType) {
                     case Basic:
@@ -63,9 +73,11 @@ public class ServerRequests<T> extends AsyncTask {
                 }
             }
 
+            // Call the callback when the request has completed
             client.newCall(reqBuilder.build()).enqueue(this.callback);
 
         } catch (Exception e) {
+            // Print the error
             e.printStackTrace();
         }
         return null;
